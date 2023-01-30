ca5d3f7e00154620145f737b50063030107ed15d
João Santos <joaosantosdev97@gmail.com>
2023-01-29 22:56:19 -0300

    Tailwind and Scrollbar config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import React from "react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+
+const chains = [arbitrum, mainnet, polygon];
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "<YOUR_PROJECT_ID>" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <WagmiConfig client={wagmiClient}>
+        <div className="h-screen">React Web3 Template</div>
+        <div className="h-screen">React Web3 Template</div>
+        <div className="h-screen">React Web3 Template</div>
+        <div className="h-screen">React Web3 Template</div>
+        <div className="h-screen">React Web3 Template</div>
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId="<YOUR_PROJECT_ID>"
+        ethereumClient={ethereumClient}
+      />
+    </React.Fragment>
   );
 }
 
